bd4a55cbcbdbd843fadc78fcca8a0f10058f2aad
GhoulMage <theghoulmage@gmail.com>
2022-11-11 10:29:59 +0100

    Startup pokemon. Run indoors. Poison Fade. B in water. Forget HM. Default Settings.
    merging with 1.3.0
    
    merging 1.5.0
    
    merging with 1.6.0

--- src/bike.c
+++ src/bike.c
@@ -1053,7 +1053,7 @@ void Bike_HandleBumpySlopeJump(void)
 
 bool32 IsRunningDisallowed(u8 metatile)
 {
-    if ((OW_RUNNING_INDOORS == GEN_3 && !gMapHeader.allowRunning) || IsRunningDisallowedByMetatile(metatile) == TRUE)
+    if (IsRunningDisallowedByMetatile(metatile) == TRUE)
         return TRUE;
 
     return FALSE;
