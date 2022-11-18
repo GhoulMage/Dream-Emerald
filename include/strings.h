dd1a6a0830d24f7456ccfcec657667c24e77caaf
GhoulMage <theghoulmage@gmail.com>
2022-11-19 00:36:36 +0100

    Added Move item functionality.
    merging with 1.5.0

--- include/strings.h
+++ include/strings.h
@@ -2163,6 +2163,7 @@ extern const u8 gText_Learned[];
 extern const u8 gText_Have[];
 extern const u8 gText_DontHave[];
 extern const u8 gText_Take[];
+extern const u8 gText_Move[];
 extern const u8 gText_Mail[];
 extern const u8 gText_Take2[];
 extern const u8 gText_Read2[];
