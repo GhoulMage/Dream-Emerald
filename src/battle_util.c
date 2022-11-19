651e6219cc2a5ca5b9de772a94dcc37562ef2a8f
GhoulMage <theghoulmage@gmail.com>
2022-11-19 11:26:45 +0100

    Fixed Overworld Snow. Hail starts when snowing.

--- src/battle_util.c
+++ src/battle_util.c
@@ -4381,6 +4381,15 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u16 ability, u8 special, u16 move
                         effect++;
                     }
                     break;
+                case WEATHER_SNOW:
+                    if (!(gBattleWeather & B_WEATHER_HAIL))
+                    {
+                        gBattleWeather = B_WEATHER_HAIL;
+                        gBattleScripting.animArg1 = B_ANIM_HAIL_CONTINUES;
+                        gBattleScripting.battler = battler;
+                        effect++;
+                    }
+                    break;
                 }
             }
             if (effect != 0)
