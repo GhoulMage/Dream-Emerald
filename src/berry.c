7b848ab05609b01628190d98602f3dfe0394bc26
GhoulMage <theghoulmage@gmail.com>
2022-11-22 18:16:00 +0100

    berries in rain will always yield max amount.

--- src/berry.c
+++ src/berry.c
@@ -14,6 +14,8 @@
 #include "text.h"
 #include "constants/event_object_movement.h"
 #include "constants/items.h"
+#include "constants/map_groups.h"
+#include "constants/weather.h"
 
 static u16 BerryTypeToItemId(u16 berry);
 static u8 BerryTreeGetNumStagesWatered(struct BerryTree *tree);
@@ -2071,7 +2073,16 @@ static u8 CalcBerryYield(struct BerryTree *tree)
 
 static u8 GetBerryCountByBerryTreeId(u8 id)
 {
-    return gSaveBlock1Ptr->berryTrees[id].berryYield;
+    struct BerryTree *tree = GetBerryTreeInfo(id);
+    const struct Berry *berry = GetBerryInfo(tree->berry);
+    //u16 currentMap = gMapHeader.regionMapSectionId;
+    //isRaining = (currentMap == MAPSEC_ROUTE_119 || currentMap == MAPSEC_ROUTE_120 || currentMap == MAPSEC_ROUTE_123);
+    bool8 isRaining = (gMapHeader.weather == WEATHER_RAIN);
+
+    if(isRaining)
+        return berry->maxYield;
+    else
+        return gSaveBlock1Ptr->berryTrees[id].berryYield;
 }
 
 static u16 GetStageDurationByBerryType(u8 berry)
