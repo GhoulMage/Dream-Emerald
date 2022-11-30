2548413c0624a24d9104d2d343e2ec15e61ced3b
GhoulMage <theghoulmage@gmail.com>
2022-11-30 17:49:32 +0100

    Dream Oldale stuff. Region map updated.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,4 +1059,7 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 
+	.include "data/scripts/underground_tunnels.inc"
+	.include "data/text/underground_tunnels.inc"
+
 	.include "data/maps/DreamOldale/scripts.inc"
