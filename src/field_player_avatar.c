614e217294bffd9a03f0eac0a8baf71b2868ec5c
GhoulMage <theghoulmage@gmail.com>
2022-12-04 21:19:57 +0100

    Leaving Surf ground event fixed.

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1694,6 +1694,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
