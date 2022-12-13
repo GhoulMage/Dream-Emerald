989cfe81ebd4ad26aca13c9a5eb8888ee152d487
GhoulMage <theghoulmage@gmail.com>
2022-12-13 14:45:14 +0100

    Double type frame animation time changed from 30 to 40. Fixed a small graphics issue (hopefully).

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -922,13 +922,15 @@ static const union AnimCmd sSpriteAnim_TypeSound[] = {
 #define PAL_TYPE_FAIRY        14
 #define PAL_TYPE_SOUND        11
 
-#define SPRITE_ANIM(type, paletteIndex)                 \
-{                                                       \
-    ANIMCMD_FRAME(TYPE_SOUND * 8, 30, FALSE, FALSE),    \
-    ANIMCMD_PALETTE(paletteIndex),                      \
-    ANIMCMD_FRAME(type * 8, 30, FALSE, FALSE),          \
-    ANIMCMD_PALETTE(PAL_TYPE_SOUND),                    \
-    ANIMCMD_JUMP(0)                                     \
+#define SPRITE_ANIM_FRAMES 40
+
+#define SPRITE_ANIM(type, paletteIndex)                              \
+{                                                                    \
+    ANIMCMD_FRAME(TYPE_SOUND * 8, SPRITE_ANIM_FRAMES, FALSE, FALSE), \
+    ANIMCMD_PALETTE(paletteIndex),                                   \
+    ANIMCMD_FRAME(type * 8, SPRITE_ANIM_FRAMES, FALSE, FALSE),       \
+    ANIMCMD_PALETTE(PAL_TYPE_SOUND),                                 \
+    ANIMCMD_JUMP(0)                                                  \
 }
 
 static const union AnimCmd sSpriteAnim_TypeSoundNormal[]    = SPRITE_ANIM(TYPE_NORMAL    ,   PAL_TYPE_NORMAL);
@@ -993,11 +995,13 @@ static const union AnimCmd *const sSpriteAnimTable_MoveTypes[NUMBER_OF_MON_TYPES
     sSpriteAnim_TypeDark,
     sSpriteAnim_TypeFairy,
     sSpriteAnim_TypeSound,
+
     sSpriteAnim_CategoryCool,
     sSpriteAnim_CategoryBeauty,
     sSpriteAnim_CategoryCute,
     sSpriteAnim_CategorySmart,
     sSpriteAnim_CategoryTough,
+
     sSpriteAnim_TypeSoundNormal,
     sSpriteAnim_TypeSoundFighting,
     sSpriteAnim_TypeSoundFlying,
@@ -1057,12 +1061,14 @@ static const u8 sMoveTypeToOamPaletteNum[NUMBER_OF_MON_TYPES + CONTEST_CATEGORIE
     [TYPE_DRAGON] =       PAL_TYPE_DRAGON   ,
     [TYPE_DARK] =         PAL_TYPE_DARK     ,
     [TYPE_FAIRY] =        PAL_TYPE_FAIRY    ,
-    [TYPE_SOUND] =        PAL_TYPE_SOUND    , // NOTE: Using an vram location that i really dont know if it is unused. hope it's ok
+    [TYPE_SOUND] =        PAL_TYPE_SOUND    , // NOTE: Using a vram location that I really dont know if it is unused. hope it's ok
+
     [NUMBER_OF_MON_TYPES + CONTEST_CATEGORY_COOL] = 13,
     [NUMBER_OF_MON_TYPES + CONTEST_CATEGORY_BEAUTY] = 14,
     [NUMBER_OF_MON_TYPES + CONTEST_CATEGORY_CUTE] = 14,
     [NUMBER_OF_MON_TYPES + CONTEST_CATEGORY_SMART] = 15,
     [NUMBER_OF_MON_TYPES + CONTEST_CATEGORY_TOUGH] = 13,
+
     [SOUND_TYPE_VARIATIONS_START + TYPE_NORMAL] =       PAL_TYPE_SOUND,
     [SOUND_TYPE_VARIATIONS_START + TYPE_FIGHTING] =     PAL_TYPE_SOUND,
     [SOUND_TYPE_VARIATIONS_START + TYPE_FLYING] =       PAL_TYPE_SOUND,
@@ -4175,7 +4181,7 @@ static u8 LoadMonGfxAndSprite(struct Pokemon *mon, s16 *state)
         if (gMain.inBattle)
         {
             HandleLoadSpecialPokePic(TRUE,
-                                     MonSpritesGfxManager_GetSpritePtr(B_POSITION_OPPONENT_LEFT),
+                                     gMonSpritesGfxPtr->sprites.ptr[B_POSITION_OPPONENT_LEFT],
                                      summary->species2,
                                      summary->pid);
         }
@@ -4184,7 +4190,7 @@ static u8 LoadMonGfxAndSprite(struct Pokemon *mon, s16 *state)
             if (gMonSpritesGfxPtr != NULL)
             {
                 HandleLoadSpecialPokePic(TRUE,
-                                         MonSpritesGfxManager_GetSpritePtr(B_POSITION_OPPONENT_LEFT),
+                                         gMonSpritesGfxPtr->sprites.ptr[B_POSITION_OPPONENT_LEFT],
                                          summary->species2,
                                          summary->pid);
             }
